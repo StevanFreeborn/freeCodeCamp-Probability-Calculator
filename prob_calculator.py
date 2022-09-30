12715f54f77cdca6d0297d34422c88c10e900675
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2022-09-30 18:00:44 +0000

    implement __init__ method for hat class

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,6 +3,23 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
+  def __init__(self, green=None, red=None, orange=None, black=None, blue=None, pink=None, striped=None):
+    self.contents = []
+  
+    balls = {
+      "green": green,
+      "red": red, 
+      "orange": orange, 
+      "black": black, 
+      "blue": blue, 
+      "pink": pink, 
+      "striped": striped
+    }
 
-
+    for key, value in balls.items():
+      if value is not None:
+        for i in range(value):
+          self.contents.append(key)
+  
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  print("experiment")
