244271e0744da9acd4adb493c9bced1cc744a5e0
Stevan Freeborn <65925598+StevanFreeborn@users.noreply.github.com>
2022-09-30 19:00:46 +0000

    passed all tests

--- prob_calculator.py
+++ prob_calculator.py
@@ -3,20 +3,10 @@ import random
 # Consider using the modules imported above.
 
 class Hat:
-  def __init__(self, green=None, red=None, orange=None, black=None, blue=None, pink=None, striped=None):
+  def __init__(self, **kwargs):
     self.contents = []
-  
-    balls = {
-      "green": green,
-      "red": red, 
-      "orange": orange, 
-      "black": black, 
-      "blue": blue, 
-      "pink": pink, 
-      "striped": striped
-    }
 
-    for key, value in balls.items():
+    for key, value in kwargs.items():
       if value is not None:
         for i in range(value):
           self.contents.append(key)
@@ -36,6 +26,23 @@ class Hat:
         drawn_balls.append(drawn_ball)
         
       return drawn_balls
+
+def has_expected_outcome(expected_balls, drawn_balls):
+  for key, value in expected_balls.items():
+    count_of_balls = drawn_balls.count(key)
+    if count_of_balls < value:
+      return False
+    
+  return True
   
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-  print("experiment")
+  num_of_expected_outcomes = 0
+  
+  for i in range(num_experiments):
+    hat_copy = copy.deepcopy(hat)
+    drawn_balls = hat_copy.draw(num_balls_drawn)
+    if has_expected_outcome(expected_balls, drawn_balls):
+      num_of_expected_outcomes += 1
+
+  return num_of_expected_outcomes / num_experiments
+      
